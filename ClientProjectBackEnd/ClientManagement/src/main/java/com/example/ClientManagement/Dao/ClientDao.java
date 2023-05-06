63b140c6a372b1de8c5bd517d4adc0206172b7a0
sebgrant0517 <122310912+sebgrant0517@users.noreply.github.com>
2023-05-06 19:27:28 -0400

    Delete ClientProjectBackEnd/ClientManagement directory

--- ClientProjectBackEnd/ClientManagement/src/main/java/com/example/ClientManagement/Dao/ClientDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.ClientManagement.Dao;
-
-import com.example.ClientManagement.Entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientDao extends JpaRepository<Client, Integer> {
-}
