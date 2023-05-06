10c74579fa848bc06f21e474412e6a71867679b5
Sebastian Grant <sebgrant0517@gmail.com>
2023-05-06 15:39:58 -0400

    Took out Spring Security for now

--- src/main/java/genspark/ClientProjectManagement/ClientProjectManagementApplication.java
+++ src/main/java/genspark/ClientProjectManagement/ClientProjectManagementApplication.java
@@ -2,6 +2,16 @@ package genspark.ClientProjectManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class ClientProjectManagementApplication {
@@ -10,4 +20,16 @@ public class ClientProjectManagementApplication {
 		SpringApplication.run(ClientProjectManagementApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedHeaders(List.of("*"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+		corsConfiguration.setMaxAge(Duration.ofMinutes(10));
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(source);
+	}
 }
+
