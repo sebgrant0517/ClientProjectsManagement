839c70a2b90219cef467c51ee88ad6d33169b9cb
sebgrant0517 <122310912+sebgrant0517@users.noreply.github.com>
2023-05-06 16:06:28 -0400

    Delete src directory

--- src/main/java/genspark/ClientProjectManagement/Dao/ClientDao.java
+++
@@ -1,9 +0,0 @@
-package genspark.ClientProjectManagement.Dao;
-
-import genspark.ClientProjectManagement.Entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientDao extends JpaRepository<Client, Integer> {
-}
--- src/main/java/genspark/ClientProjectManagement/Dao/ProjectDao.java
+++
@@ -1,9 +0,0 @@
-package genspark.ClientProjectManagement.Dao;
-
-import genspark.ClientProjectManagement.Entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectDao extends JpaRepository<Project, Integer> {
-}
